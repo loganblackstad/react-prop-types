031cbf7d53e11dd4487e9e3d63535e4791b75642
Logan Blackstad <logan.blackstad@gmail.com>
2020-09-29 15:18:10 -0400

    covered all react styling methods except wrapper HOC styling methodology

--- src/App.js
+++ src/App.js
@@ -10,11 +10,23 @@ import { cardBasic, cardDanger, cardFull } from "./settings";
 import "./style.css";
 import ButtonOneStyles from "./styles/ButtonOneStyles";
 import mystyle from './styles/mystyle.module.css';
-
+import './styles/mysass.scss';
 
 class StyledClassCmp extends React.Component {
   render() {
     return <h2 className={mystyle.bigblue}>{`The component must import css from <name>.module.css`}</h2>;
+  }
+}
+
+class MySassStyleCmp extends React.Component {
+  render() {
+    return (
+      <div>
+        <br />
+        <p style={{ textAlign: 'center' }}>{`add css styling from ./styles/mssass.scss`}</p>
+        <h3>This is styled with Scss</h3>
+      </div>
+    );
   }
 }
 
@@ -35,6 +47,7 @@ function App() {
       <br />
       <p style={{ textAlign: 'center' }}>{`add css styling from ./styles/mystyle.module.css`}</p>
       <StyledClassCmp />
+      <MySassStyleCmp />
     </div>
   );
 }
