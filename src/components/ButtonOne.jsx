4a684d0646348faa7cb50f8960144f8d8c43b21b
Logan Blackstad <logan.blackstad@gmail.com>
2020-09-29 14:22:22 -0400

    added multiple ways to style to buttons

--- src/components/ButtonOne.jsx
+++ src/components/ButtonOne.jsx
@@ -3,6 +3,15 @@ import ButtonOneStyles from "../styles/ButtonOneStyles";
 
 const b1s = ButtonOneStyles;
 
+const OtherStyle = {
+  other: {
+    backgroundColor: '#dbf0db',
+    color: '#6b6a6f',
+    padding: '10px',
+  }
+}
+
+
 const ButtonOne = (props) => {
   console.log(props);
   return (
@@ -12,15 +21,15 @@ const ButtonOne = (props) => {
       <p>no styling: </p>
       <button style={props.styles}>I am ButtonOne</button>
 
-      <p>inline styles overwrite imported styles: </p>
-      {/* inline styles over-write imported styles */}
-      <button style={b1s.default} style={{ color: 'red' }}>I am ButtonOne</button>
-
       <p>imported styles: "ButtonOneStyles.default"</p>
       <button style={b1s.default}>I am ButtonOne</button>
 
-      <p>no styling: </p>
-      <button>I am ButtonOne</button>
+      <p>inline styles overwrite imported styles: &nbsp;&nbsp; style = {`{{ color: 'red' }}`}</p>
+      {/* inline styles over-write imported styles */}
+      <button style={b1s.default} style={{ color: 'red' }}>I am ButtonOne</button>
+
+      <p>styling defined in the same file: "OtherStyle.other"</p>
+      <button style={OtherStyle.other}>I am ButtonOne</button>
 
     </div>
   )
