86b5263f9ae0914125ff591e58164418e3431583
Logan Blackstad <logan.blackstad@gmail.com>
2020-09-30 10:58:41 -0400

    added comment to withHOC.jsx

--- src/components/withHOC.jsx
+++ src/components/withHOC.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+
+// the name attr is added to props of the OriginalComponent
+
 const UpdatedComponent = OriginalComponent => {
   class NewComponent extends React.Component {
     render() {
